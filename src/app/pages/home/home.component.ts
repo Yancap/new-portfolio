29ee2f476bc6a9f6fb247e980f9ac830bb242b09
Yancap <yangabriel3001f@gmail.com>
2023-12-04 19:37:47 -0300

    final version

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,24 +11,7 @@ import {
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('changePage', [
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.8,
-        backgroundColor: 'blue'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ])
-    ])
-  ]
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   constructor(
